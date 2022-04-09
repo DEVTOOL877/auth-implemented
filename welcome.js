ac60b4a9640181759d244033162a5c7953cdca9d
HJX <73078933+DEVTOOL877@users.noreply.github.com>
2022-04-09 11:56:36 +0300

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,12 +1,18 @@
+const whenSignedOut = document.getElementById('whenSignedOut');
+const whenSignedIn = document.getElementById('whenSignedIn');
+
 firebase.auth().onAuthStateChanged((user)=>{
     if(!user){
-        location.replace("index.html")
+        whenSignedIn.hidden = true;
+          whenSignedOut.hidden = false;
     }else{
         document.getElementById("user").innerHTML = "Hello, "+user.email
+        whenSignedIn.hidden = false;
+          whenSignedOut.hidden = true;
     }
 })
 
 
 function logout(){
-    firebase.auth().signOut()
+    // firebase.auth().signOut()
 }
